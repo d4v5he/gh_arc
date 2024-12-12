e0f8d702cd75657d39d0bf1aeae6f9a847e8948f
d4v5he <d4v5he@gmail.com>
2024-12-12 23:47:36 +0200

    WIP

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "main" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  subnet_id       = var.subnet_id
-  security_groups = var.security_group_ids
-  user_data       = var.user_data
-  key_name          = var.ssh_key_name
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.ssh_key_name
+  user_data              = var.user_data
 
   tags = {
     Name = var.name
